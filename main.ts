3b06113e3c71b7ca5d47084b589102865b8ebfbe
Finn-Smith <59915197+Finn-Smith@users.noreply.github.com>
2020-04-30 16:32:07 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+let mySprite = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . b b b c b b f f . . . . . . 
+. . b c b b c b f f . . . . . . 
+f b b b f b b b b b . . . . . . 
+f f c b f f b c b b . . . . . . 
+f f b c f f b b b c . . f f . . 
+f f b b f f c b b b . f f f f . 
+f f . . f f b c b b f f . . f . 
+f f . . f f b b b c f f . . f f 
+f f . . f f c b 4 4 4 4 . . f f 
+f f . . f f b c 2 2 2 2 . . f f 
+f f . . f f . . b b b c . . f f 
+f f . . f f . . b c b b . . f f 
+. f . . . f . . b b b b . . . f 
+. f . . . f . . . f . . . . . f 
+`, SpriteKind.Player)
